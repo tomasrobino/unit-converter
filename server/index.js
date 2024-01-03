98e4644ab0f3fd2b69273d99308ecbbb95d09572
tomasrobino <tomaserobino@gmail.com>
2024-01-03 14:07:12 +0100

    testing of server and mysql connection

--- server/index.js
+++ server/index.js
@@ -3,3 +3,30 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 3008;
 const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello" })
+});
+
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'conversionuser',
+  password: 'password',
+  database: 'conversions'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
+
+app.listen(PORT, () => {
+    console.log("server listening at port "+PORT);
+});
