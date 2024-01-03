d7b663da345ea656427dc59c3a27d895114b86e1
tomasrobino <tomaserobino@gmail.com>
2024-01-03 15:11:18 +0100

    switched mysql2 to using Promises

--- server/index.js
+++ server/index.js
@@ -8,24 +8,25 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello" })
 });
 
-const mysql = require('mysql2')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'conversionuser',
-  password: 'password',
-  database: 'conversions'
-})
+async function main() {
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'conversionuser',
+        password: 'password',
+        database: 'conversions'
+    })
 
-connection.connect()
+    try {
+        const [results, fields] = await connection.query('SELECT * from conversions');
+        console.log(results);
+        console.log(fields);
+    } catch(err) {
+        console.log(err);
+    }
+}
 
-connection.query('SELECT * from conversions', (err, rows, fields) => {
-  if (err) throw err
-
-  console.log('The solution is: ', rows)
-})
-
-connection.end()
-
+main();
 
 app.listen(PORT, () => {
     console.log("server listening at port "+PORT);
