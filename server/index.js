d4df2865900cf660301f3bd6382f02ba4b0e7a1f
tomasrobino <tomaserobino@gmail.com>
2024-01-03 14:11:20 +0100

    testing database SELECT

--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+connection.query('SELECT * from conversions', (err, rows, fields) => {
   if (err) throw err
 
-  console.log('The solution is: ', rows[0].solution)
+  console.log('The solution is: ', rows)
 })
 
 connection.end()
