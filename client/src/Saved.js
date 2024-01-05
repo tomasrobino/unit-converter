98d4d831063fb4fd1ae9a45e16dbf0b0b8d33306
tomasrobino <tomaserobino@gmail.com>
2024-01-05 17:18:42 +0100

    deletion working (without updating database)

--- client/src/Saved.js
+++ client/src/Saved.js
@@ -2,18 +2,19 @@ import equis from './equis.svg';
 
 export default function Saved(props) {
     function handleDeletion(e) {
-        let a = [...props.saved];
-        a.splice(a.findIndex((element) => element === e.target.parentNode.firstChild.textContent), 1);
-        props.setSaved(a);
-        console.log(a);
+        if (e.target.tagName === "IMG") {
+            let a = [...props.saved];
+            a.splice(a.findIndex((element) => element.id === parseInt(e.currentTarget.id)), 1);
+            props.setSaved(a);
+        }
     }
     
     let saved = [];
     if (Array.isArray(props.saved)){
-        props.saved.forEach((_element, i) => {
+        props.saved.forEach((element, i) => {
             saved.push(
-                <div key={props.saved[i].id} id={props.saved[i].id} onClick={handleDeletion} className='savedBox'>
-                    <p>{props.saved[i].first} {props.saved[i].type} {props.saved[i].second}</p>
+                <div key={element.id} id={element.id} onClick={handleDeletion} className='savedBox'>
+                    <p>{element.first} {element.type} {element.second}</p>
                     <img src={equis} alt="" className='deleteImg'/>
                 </div>
             )
